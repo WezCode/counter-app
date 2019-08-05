a062861ad17d2df070570905257c227dd641f2fe
WezCode <wesley.rmit@gmail.com>
2019-08-05 19:12:13 +1000

    Basic setup

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+//counter module
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return <h1>Hello dWorld</h1>
+    }
+}
+
+export default Counter;
